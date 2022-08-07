c9dc0a90ba7c1aeff32bbe362ccebfe4804d2fcd
louiml.net <99025570+loui-dev@users.noreply.github.com>
2022-08-07 22:36:09 +0300

    Create j=i.rb

---
+++ j=i.rb
@@ -0,0 +1,10 @@
+def insertionSort(array)
+  for i in 1..array.length-1
+    j = i
+    while j > 0 && array[j-1] > array[j]
+      array[j-1], array[j] = array[j], array[j-1]
+      j -= 1
+    end
+  end
+  return array
+end
