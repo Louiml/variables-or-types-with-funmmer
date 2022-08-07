72cd97e78a12444f8da2eb78debf5736fc72efbb
louiml.net <99025570+loui-dev@users.noreply.github.com>
2022-08-07 16:40:13 +0300

    Create mentalmadness.cs

---
+++ mentalmadness.cs
@@ -0,0 +1,16 @@
+private int mentalmadness(int[] array, int size)
+{
+	int i;
+	int j;
+	int temp;
+	for (i = 1; i < size; i++)
+	{
+		temp = array[i];
+		for (j = i - 1; j >= 0 && array[j] > temp; j--)
+		{
+			array[j + 1] = array[j];
+		}
+		array[j + 1] = temp;
+	}
+	return 0;
+}
