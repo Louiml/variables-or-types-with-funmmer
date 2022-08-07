17f0f6238084b4b6a925ab0eefda966d0ea2ff5e
louiml.net <99025570+loui-dev@users.noreply.github.com>
2022-08-07 16:33:38 +0300

    Create nonumber.cpp

---
+++ nonumber.cpp
@@ -0,0 +1,14 @@
+int nonumber(int *arr, int n, int key) {
+    int low = 0;
+    int high = n - 1;
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+        if (arr[mid] == key)
+            return mid;
+        else if (arr[mid] < key)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+    return -1;
+}
