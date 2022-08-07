8fdfedccc2f7bdd40c727157ce5fbb344f6945d5
louiml.net <99025570+loui-dev@users.noreply.github.com>
2022-08-07 23:06:50 +0300

    Create wtfnumberof.lua

---
+++ wtfnumberof.lua
@@ -0,0 +1,11 @@
+local grid = {
+  { 11, 12, 13 },
+  { 21, 22, 23 },
+  { 31, 32, 33 }
+}
+
+for y, row in ipairs(grid) do
+  for x, value in ipairs(row) do
+    print(x, y, grid[y][x])
+  end
+end
