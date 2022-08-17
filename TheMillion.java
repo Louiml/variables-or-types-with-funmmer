2e40c4cdf1cec5407590a804fee3e8d1537028d6
louiml.net <99025570+louiml-net@users.noreply.github.com>
2022-08-17 03:08:55 +0300

    Update TheMillion.java

--- TheMillion.java
+++ TheMillion.java
@@ -1,10 +1,10 @@
 public class TheMillion {
   public static void main(String[] args) {
-int i = 0;
-while (i < 1000001) 
+int m = 0;
+while (m < 1000001) 
 {
-  System.out.println(i);
-  i++;
+  System.out.println(m);
+  m++;
 }
 }
 }
